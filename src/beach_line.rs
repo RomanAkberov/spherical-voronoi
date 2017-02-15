f859e9fc04b76338c79fa0581ead26936462f4b2
Roman Akberov <r.k.akberov@gmail.com>
2017-02-15 22:13:54 +0100

    Simplify and rearrange BeachLine

--- src/beach_line.rs
+++ src/beach_line.rs
@@ -30,7 +30,8 @@ pub struct BeachLine {
 }
 
 impl BeachLine {
-    pub fn insert(&mut self, cell: Cell, sites: &[SiteEvent]) -> Arc {
+    pub fn insert(&mut self, cell: Cell, sites: &[SiteEvent]) -> Arc {        
+        let new = self.create_arc(cell);
         if self.len > 1 {
             let mut current = self.head;
             let mut level = HEIGHT - 1;
@@ -63,16 +64,13 @@ impl BeachLine {
             let current_cell = self.cell(current);
             let twin = self.create_arc(current_cell);
             let prev = self.prev(current);  
-            self.insert_after(prev, twin);     
-            let new = self.create_arc(cell);
-            self.insert_after(twin, new);
+            self.insert_between(twin, prev, current);
+            self.insert_between(new, twin, current);
             self.add_skips(new, &mut skips);
             self.add_skips(twin, &mut skips);
-            new
-        } else if self.len > 0 {        
-            let new = self.create_arc(cell);
+        } else if self.len > 0 {
             let head = self.head;
-            self.insert_after(head, new);
+            self.insert_between(new, head, head);
             let height = self.insertion_height();
             self.levels[height - 1] += 1;
             for level in 0..height {
@@ -81,23 +79,93 @@ impl BeachLine {
                 self.set_prev_skip(head, level, new);
                 self.set_next_skip(head, level, new);
             }
-            new
         } else {
-            let new = self.create_arc(cell);
+            self.insert_between(new, new, new);
             {
                 let data = &mut self.arcs[new];
-                data.prev = new;
-                data.next = new;
                 data.prev_skips = [new; HEIGHT];
                 data.next_skips = [new; HEIGHT];
             }
             self.head = new;
             self.levels[HEIGHT - 1] += 1;
-            self.len += 1;
-            new
         }
+        new
     }
     
+    pub fn neighbors(&self, arc: Arc) -> (Arc, Arc) {
+        let data = &self.arcs[arc];
+        (data.prev, data.next)
+    }
+
+    pub fn remove(&mut self, arc: Arc) {
+        let head = self.head;
+        if arc == head {
+            let next_skip = self.next_skip(self.head, HEIGHT - 1);
+            if next_skip != self.head {
+                self.head = next_skip;
+            } else {
+                let next = self.next(self.head);
+                let height = self.height(next);
+                self.levels[height - 1] -= 1;
+                self.levels[HEIGHT - 1] += 1;
+                for level in height..HEIGHT {
+                    let next_skip = self.next_skip(self.head, level);
+                    self.set_prev_skip(next_skip, level, next);
+                    self.set_next_skip(next, level, next_skip);
+                    self.set_prev_skip(next, level, head);
+                    self.set_next_skip(head, level, next);
+                }
+                self.head = next;
+            }
+        }
+        self.remove_skips(arc);
+        let (prev, next) = self.neighbors(arc);
+        self.arcs[prev].next = next;
+        self.arcs[next].prev = prev;
+        self.len -= 1;
+    }
+
+    pub fn cell(&self, arc: Arc) -> Cell {
+        self.arcs[arc].cell
+    }
+    
+    pub fn start(&self, arc: Arc) -> Id<Vertex> {
+        self.arcs[arc].start    
+    }
+    
+    pub fn set_start(&mut self, arc: Arc, start: Id<Vertex>) {
+        self.arcs[arc].start = start;
+    }
+    
+    pub fn center(&self, arc: Arc) -> Option<Vector3<f64>> {
+        self.arcs[arc].center
+    }
+
+    pub fn attach_circle(&mut self, arc: Arc, center: Vector3<f64>) {
+        self.arcs[arc].center = Some(center);
+    }
+
+    pub fn detach_circle(&mut self, arc: Arc) {
+        self.arcs[arc].center = None;
+    }
+
+
+    pub fn prev(&self, arc: Arc) -> Arc {
+        self.arcs[arc].prev
+    }
+
+
+    pub fn next(&self, arc: Arc) -> Arc {
+        self.arcs[arc].next
+    }
+
+    pub fn clear(&mut self) {
+        self.arcs.clear();
+        self.head = Arc::invalid();
+        self.len = 0;
+        self.levels = [0; HEIGHT];
+    }
+
     fn create_arc(&mut self, cell: Cell) -> Arc {
         self.arcs.push(ArcData {
             cell: cell,
@@ -112,24 +180,10 @@ impl BeachLine {
         })
     }
 
-    // fn range_end(&mut self, arc1: Arc, arc2: Arc, site: &SiteEvent, diagram: &Diagram) -> f64 {
-    //     if self.arcs[arc1].scan < site.theta.value {
-    //         self.arcs[arc1].end = self.intersect_with_next(arc1, site, diagram);
-    //         self.arcs[arc2].end = self.intersect_with_next(arc2, site, diagram);
-    //         self.arcs[arc1].scan = site.theta.value;
-    //         self.arcs[arc2].scan = site.theta.value;
-    //     }
-    //     self.arcs[arc1].end
-    // }
-
     fn skips(&self, arc: Arc, level: usize) -> (Arc, Arc) {
         let data = &self.arcs[arc];
         (data.prev_skips[level], data.next_skips[level])
     }
-
-    // fn prev_skip(&self, arc: Arc, level: usize) -> Arc {
-    //     self.arcs[arc].prev_skips[level]
-    // }
 
     fn set_prev_skip(&mut self, arc: Arc, level: usize, prev: Arc) {
         self.arcs[arc].prev_skips[level] = prev;
@@ -186,46 +240,12 @@ impl BeachLine {
         phi * 2.0 * PI
     }
 
-    pub fn insert_after(&mut self, prev: Arc, arc: Arc) {
-        let next = self.next(prev);
+    fn insert_between(&mut self, arc: Arc, prev: Arc, next: Arc) {
         self.arcs[arc].prev = prev;
         self.arcs[arc].next = next;
         self.arcs[prev].next = arc;
         self.arcs[next].prev = arc;
         self.len += 1;
-    }
-    
-    pub fn neighbors(&self, arc: Arc) -> (Arc, Arc) {
-        let data = &self.arcs[arc];
-        (data.prev, data.next)
-    }
-
-    pub fn remove(&mut self, arc: Arc) {
-        let head = self.head;
-        if arc == head {
-            let next_skip = self.next_skip(self.head, HEIGHT - 1);
-            if next_skip != self.head {
-                self.head = next_skip;
-            } else {
-                let next = self.next(self.head);
-                let height = self.height(next);
-                self.levels[height - 1] -= 1;
-                self.levels[HEIGHT - 1] += 1;
-                for level in height..HEIGHT {
-                    let next_skip = self.next_skip(self.head, level);
-                    self.set_prev_skip(next_skip, level, next);
-                    self.set_next_skip(next, level, next_skip);
-                    self.set_prev_skip(next, level, head);
-                    self.set_next_skip(head, level, next);
-                }
-                self.head = next;
-            }
-        }
-        self.remove_skips(arc);
-        let (prev, next) = self.neighbors(arc);
-        self.arcs[prev].next = next;
-        self.arcs[next].prev = prev;
-        self.len -= 1;
     }
 
     fn remove_skips(&mut self, arc: Arc) {
@@ -245,47 +265,6 @@ impl BeachLine {
             }
         }
         HEIGHT
-    }
-    
-    pub fn cell(&self, arc: Arc) -> Cell {
-        self.arcs[arc].cell
-    }
-    
-    pub fn start(&self, arc: Arc) -> Id<Vertex> {
-        self.arcs[arc].start    
-    }
-    
-    pub fn set_start(&mut self, arc: Arc, start: Id<Vertex>) {
-        self.arcs[arc].start = start;
-    }
-    
-    pub fn center(&self, arc: Arc) -> Option<Vector3<f64>> {
-        self.arcs[arc].center
-    }
-
-    pub fn attach_circle(&mut self, arc: Arc, center: Vector3<f64>) {
-        self.arcs[arc].center = Some(center);
-    }
-
-    pub fn detach_circle(&mut self, arc: Arc) {
-        self.arcs[arc].center = None;
-    }
-
-
-    pub fn prev(&self, arc: Arc) -> Arc {
-        self.arcs[arc].prev
-    }
-
-
-    pub fn next(&self, arc: Arc) -> Arc {
-        self.arcs[arc].next
-    }
-
-    pub fn clear(&mut self) {
-        self.arcs.clear();
-        self.head = Arc::invalid();
-        self.len = 0;
-        self.levels = [0; HEIGHT];
     }
 
     fn insertion_height(&self) -> usize {
